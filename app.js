db3a6799b19f2f690dd650485347e9369607a3df
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2023-08-16 20:44:06 +0530

    mongoose installed

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const tourRouter = require('./routes/toursRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const tourRouter = require('./routes/toursRoutes')
-const userRouter = require('./routes/userRoutes')
-
-const app =express();
+const app = express();
 
 //Middlewears
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
-app.use((req,res,next)=>{
-    //console.log('hello from middlewear');
-    next();
-})
+app.use((req, res, next) => {
+  console.log('hello from middlewear');
+  next();
+});
 
-app.use((req,res,next)=>{
-    req.requestTime = new Date().toISOString();
-    next();
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 //Route
-
 
 //const userRouter = express.Router();
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
