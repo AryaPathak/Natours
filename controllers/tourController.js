2be3aa7fc76322715660223f046a95326de9567a
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2023-08-18 19:16:18 +0530

    DB Attached

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,8 @@
-const fs = require('fs');
+const express = require('express');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
-exports.checkID = (req, res, next, val)=>{
-    console.log(`Tour id is: ${val}`);
-    if(req.params.id*1>tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    next();
-}
+
 
 exports.checkbody = (req, res, next)=>{
     if(!req.body.name || !req.body.price){
@@ -29,10 +18,7 @@ exports.checkbody = (req, res, next)=>{
 exports.getAllTours = (req, res)=>{
     res.status(200).json({
         status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
+        requestedAt: req.requestTime
     })
 }
 
@@ -42,37 +28,19 @@ exports.getTour = (req, res)=>{
     console.log(req.params);
     const id = req.params.id*1;
 
-    const tour = tours.find(el=>el.id===id);
-    // if(id>tours.lengh){
     
 
     
-    res.status(200).json({
-        status: 'success',
-        
-        data: {
-            tour
-        }
-    })
+    
+
 }
 
 
 exports.createTour = (req, res)=>{
     //console.log(req.body);
-    const newId = tours[tours.length-1].id + 1;
-    const newTour = Object.assign({id:newId}, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err=>{
-        res.status(201).json({
-            status: 'success',
-            data:{
-                tour: newTour
-            }
-        })
-    }
-    ); 
+    res.status(201).json({
+        status: 'success'
+    })
     
 }
 
