232254d65a3cd15b177e916fc27fe8ac3dab837b
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2024-01-07 22:17:30 +0530

    auth

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+const User = require("../models/userModel");
+
+exports.signup = async(req, res, next) => {
+    const newUser = await User.create(req.body);
+    try{
+        res.status(201).json({
+            status: 'success',
+            data:{
+                user: newUser
+            }
+        })
+
+    }catch(err){
+        res.status(404).json({
+        status: 'fail',
+        message: err
+        })
+    
+    }
+}
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid string']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8
+    },
+    passwordConfirm:{
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function(el){
+                return el === this.password;
+            },
+            message: "Password not same"
+        }
+    }
+});
+
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
